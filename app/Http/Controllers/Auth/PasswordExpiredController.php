507494496ae4f7ec921c9c886c1a3d3746b4ddd3
Ama <amangeldiyev.amantur@gmail.com>
2020-05-18 16:44:05 +0500

    old passwords

--- app/Http/Controllers/Auth/PasswordExpiredController.php
+++ app/Http/Controllers/Auth/PasswordExpiredController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Carbon\Carbon;
+use DB;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 
@@ -34,6 +35,19 @@ class PasswordExpiredController extends Controller
         if (!Hash::check($request->current_password, $request->user()->password)) {
             return redirect()->back()->withErrors(['current_password' => 'Current password is not correct']);
         }
+
+        // Deny usage of old password
+        foreach (DB::table('old_passwords')->where('user_id', $request->user()->id)->get() as $password) {
+            if (Hash::check($request->password, $password->hash)) {
+                return redirect()->back()->withErrors(['password' => 'You can\'t use old password']);
+            }
+        }
+
+        // Save old password
+        DB::table('old_passwords')->insert([
+            'user_id' => $request->user()->id,
+            'hash' => $request->user()->password
+        ]);
 
         $request->user()->update([
             'password' => bcrypt($request->password),
