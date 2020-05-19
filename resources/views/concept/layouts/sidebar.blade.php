3e17fab131d6b54acce9ad1b31f9f5045df1e936
Ama <amangeldiyev.amantur@gmail.com>
2020-05-19 15:56:15 +0500

    wip

--- resources/views/concept/layouts/sidebar.blade.php
+++ resources/views/concept/layouts/sidebar.blade.php
@@ -170,15 +170,6 @@
     </div>
 </div>
 
-<div class="toast">
-    <div class="toast-header">
-      Toast Header
-    </div>
-    <div class="toast-body">
-      Some text inside the toast body
-    </div>
-</div>
-
 <script>
     setInterval(() => {
 
