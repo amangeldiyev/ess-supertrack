f46a4ef50cf629d2a14c7aae280c8d8ba4f96cbf
Ama <amangeldiyev.amantur@gmail.com>
2020-05-21 15:21:07 +0500

    pagination

--- app/Http/Controllers/PassengerController.php
+++ app/Http/Controllers/PassengerController.php
@@ -18,7 +18,7 @@ class PassengerController extends Controller
      */
     public function index()
     {
-        $passengers = Passenger::filterByCompany()->with('company')->limit(10000)->get();
+        $passengers = Passenger::filterByCompany()->with('company')->paginate(10)->get();
 
         return view('concept.passenger.index', compact('passengers'));
     }
